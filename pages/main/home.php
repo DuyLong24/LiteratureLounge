5aa2c703536ba70a6ed2f9bb521aef3225607acd
DuyLong24 <ngolong2k3@gmail.com>
2024-08-21 23:27:06 +0700

    first commit

---
+++ pages/main/home.php
@@ -0,0 +1,163 @@
+<?php
+    
+?>
+<div>
+    <div id="slides" class="carousel slide" data-ride="carousel">
+        <ul class="carousel-indicators">
+          <li data-target="#slides" data-slide-to="0" class="active"></li>
+          <li data-target="#slides" data-slide-to="1"></li>
+          <li data-target="#slides" data-slide-to="2"></li>
+        </ul>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="intro-slide" src="./assets/images/banners/main.jpg" alt="banner">
+            <div class="carousel-caption d-none d-md-block">
+              <p class="font-weight-bold" style="color: white; font-size: 20px;"></p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="intro-slide" src="./assets/images/banners/banner0.jpg" alt="banner">
+            <div class="carousel-caption d-none d-md-block">
+              <p class="font-weight-bold" style="color: white; font-size: 20px;">Là cầu nối giữa độc giả Việt Nam với nền văn hóa đọc mênh mông của thế giới</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="intro-slide" src="./assets/images/banners/banner3.jpg" alt="banner">
+            <div class="carousel-caption d-none d-md-block">
+              <p class="font-weight-bold">Your Literary Adventure Begins Here.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section class="section-1">
+          <div class="container text-center">
+              <div class="row">
+                  <div class="col-md-6 col-12">
+                      <div class="pray">
+                          <img
+                              src="./assets/images/banners/banner4.jpg"
+                              alt="Pray"
+                              class=""
+                          />
+                      </div>
+                  </div>
+                  <div class="col-md-6 col-12">
+                      <div class="panel text-left">
+                          <h1> Nhã Nam</h1>
+                          <p class="pt-4">
+                            Chào mừng bạn đến với hiệu sách trực tuyến của chúng tôi, nơi thế giới sách trở nên sống động. 
+                            Chúng tôi cung cấp một bộ sưu tập đá quý văn học đa dạng,
+                            từ những tác phẩm kinh điển vượt thời gian đến những cuốn sách bán chạy nhất đương đại.
+                          </p>
+                          <p>
+                          Hãy tìm hiểu, khám phá và để trí tưởng tượng của bạn bay bổng. Giao diện thân thiện với người dùng và đặt hàng dễ dàng, lần đọc tuyệt vời tiếp theo của bạn chỉ bằng một cú nhấp chuột. Hãy cùng chúng tôi tôn vinh sự kỳ diệu của sách.
+                          </p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
+      <!-- Section 4 -->
+      <section class="section-4">
+          <div class="container text-center">
+              <h1 class="text-dark">What our Customer's Say about us</h1>
+              <p class="font-italic">Feedback from Our Loyal Customers</p>
+          </div>
+          <div class="team row">
+              <div class="col-md-4 col-12 text-center">
+                  <div class="card mr-2 d-inline-block shadow-lg">
+                      <div class="card-img-top">
+                          <img
+                              src="./assets/images/banners/feedback1.jpg"
+                              class="img-fluid p-4"
+                              alt=""
+                          />
+                      </div>
+                      <div class="card-body">
+                          <h3 class="card-title">Blalock Jolene</h3>
+                          <p class="card-text">
+                            "I'm really pleased with this website! The user interface is easy to navigate,
+                             and finding books is quick and simple. 
+                             I found my favorite books easily."
+                          </p>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-4 col-12">
+                  <div
+                      id="carouselExampleControls"
+                      class="carousel slide"
+                      data-ride="carousel"
+                  >
+                      <div class="carousel-inner text-center">
+                          <div class="carousel-item active">
+                              <div
+                                  class="card mr-2 d-inline-block shadow"
+                              >
+                                  <div class="card-img-top">
+                                      <img
+                                          src="./assets/images/banners/feedback2.jpg"
+                                          class="img-fluid p-4"
+                                          alt=""
+                                      />
+                                  </div>
+                                  <div class="card-body">
+                                      <h3 class="card-title">
+                                          Allen Agnes
+                                      </h3>
+                                      <p class="card-text">
+                                      "Great customer service. 
+                                      They helped me resolve an issue with my order promptly and professionally. 
+                                      I highly appreciate their professionalism."
+                                      </p>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="carousel-item">
+                              <div
+                                  class="card d-inline-block mr-2 shadow"
+                              >
+                                  <div class="card-img-top">
+                                      <img
+                                          src="./assets/images/banners/feedback3.jpg"
+                                          class="img-fluid p-4"
+                                          alt=""
+                                      />
+                                  </div>
+                                  <div class="card-body">
+                                      <h3 class="card-title">
+                                          Amiel Barbara
+                                      </h3>
+                                      <p class="card-text">
+                                        "This website is a book lover's dream. It's easy to use,
+                                         offers a wide range of books, and delivers quickly. 
+                                         I'm a happy customer!"
+                                      </p>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-4 col-12 text-center">
+                  <div class="card mr-2 d-inline-block shadow-lg">
+                      <div class="card-img-top">
+                          <img
+                              src="./assets/images/banners/feedback4.jpg"
+                              class="img-fluid p-4"
+                              alt=""
+                          />
+                      </div>
+                      <div class="card-body">
+                          <h3 class="card-title">Olivia Louis</h3>
+                          <p class="card-text">
+                          "Fast delivery and careful packaging. 
+                          The books arrived in good condition, without any damage. 
+                          I'll definitely come back to purchase books here in the future."
+                          </p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>     
+</div>
